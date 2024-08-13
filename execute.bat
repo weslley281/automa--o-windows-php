832e7f1a6d4fe15268117311c8c30df8b67ebf73
Weslley Henrique Vieira Ferraz <weslleyhenrique800@gmail.com>
2024-08-13 15:10:12 -0400

    Update execute.bat

--- execute.bat
+++ execute.bat
@@ -1,8 +1,9 @@
 @echo off
 rem Chama o script PHP para agendar o desligamento
-php C:\automacao windows php\abrir_olimpobasic.php
-php C:\automacao windows php\abrir_sites.php
-
+php C:\automacao_windows_php\abrir_olimpobasic.php
+timeout /t 2 /nobreak >nul
+php C:\automacao_windows_php\abrir_sites.php
+timeout /t 2 /nobreak >nul
+php C:\automacao_windows_php\desligar.php
 rem Espera uma tecla ser pressionada antes de fechar a janela
 pause
-
